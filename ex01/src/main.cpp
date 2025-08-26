6829fcb7406405ecfe4e49afef4baf75cf7ef1ab
Tiago Antunes <tfa.antunes@gmail.com>
2025-08-26 15:24:47 +0100

    fix: completed tests for ex01

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -13,6 +13,7 @@
 #include "../Include/includes.hpp"
 #include <cstdlib>
 #include <ctime>
+#include <exception>
 
 void big_test(void) {
   unsigned int size = 2000000;
@@ -25,23 +26,69 @@ void big_test(void) {
 
   sp.fillSpan(arr, arr + size);
 
-  std::cout << sp.shortestSpan() << std::endl;
-  std::cout << sp.longestSpan() << std::endl;
+  std::cout << BLUE << "Shortest span: " << END << sp.shortestSpan()
+            << std::endl;
+  std::cout << BLUE << "Longest span: " << END << sp.longestSpan() << std::endl;
 }
 
 int main(void) {
   Span sp = Span(5);
 
+  std::cout << YELLOW << "SMALL ARRAY TESTS" << END << std::endl;
+  std::cout << std::endl;
+
+  try {
+    sp.shortestSpan();
+  } catch (std::exception &e) {
+    std::cout << RED << "Shortest Span without numbers stored: " << END
+              << e.what() << std::endl;
+  }
+
+  try {
+    sp.longestSpan();
+  } catch (std::exception &e) {
+    std::cout << RED << "Longest Span without numbers stored: " << END
+              << e.what() << std::endl;
+  }
+
   sp.addNumber(6);
+
+  try {
+    sp.shortestSpan();
+  } catch (std::exception &e) {
+    std::cout << RED << "Shortest Span with 1 number stored: " << END
+              << e.what() << std::endl;
+  }
+
+  try {
+    sp.longestSpan();
+  } catch (std::exception &e) {
+    std::cout << RED << "Longest Span with 1 number stored: " << END << e.what()
+              << std::endl;
+  }
+
   sp.addNumber(3);
   sp.addNumber(17);
   sp.addNumber(9);
   sp.addNumber(11);
 
-  std::cout << sp.shortestSpan() << std::endl;
-  std::cout << sp.longestSpan() << std::endl;
+  try {
+    sp.addNumber(100);
+  } catch (std::exception &e) {
+    std::cout << RED
+              << "Trying to add a new number after exceeding capacity: " << END
+              << e.what() << std::endl;
+  }
+
   std::cout << std::endl;
 
+  std::cout << BLUE << "Shortest span: " << END << sp.shortestSpan()
+            << std::endl;
+  std::cout << BLUE << "Longest span: " << END << sp.longestSpan() << std::endl;
+  std::cout << std::endl;
+
+  std::cout << YELLOW << "BIG ARRAY TESTS" << END << std::endl;
+  std::cout << std::endl;
   big_test();
 
   return 0;
